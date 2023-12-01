fb2c03bbc102442d6d2d075a1b21c2a139aeb9e4
Adilson <didasoueudida@gmail.com>
2023-12-01 10:35:06 -0300

    removendo import

--- authuser/src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -3,6 +3,8 @@ package com.ead.authuser.controllers;
 import com.ead.authuser.clients.CourseClient;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
