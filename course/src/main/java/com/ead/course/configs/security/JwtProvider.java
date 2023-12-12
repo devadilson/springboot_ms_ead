7d762dc6b4999d9e5e5520b35bdd747cb71bcaec
Adilson <didasoueudida@gmail.com>
2023-12-12 11:53:35 -0300

    feat: Implements JWT Authentication e Authorization in MS Course

---
+++ course/src/main/java/com/ead/course/configs/security/JwtProvider.java
@@ -0,0 +1,71 @@
+package com.ead.course.configs.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+;
+
+@Log4j2
+@Component
+public class JwtProvider {
+
+    @Value(value = "${ead.auth.jwtSecret}")
+    private String jwtSecret;
+
+    public String getSubjectJwt(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    // TODO: AJustar validacao do Usuário
+    public boolean validateJwt(String authToken) {
+        // return !isTokenExpired(authToken);
+        try {
+            Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(authToken);
+            return true;
+        } catch (JwtException  e) {
+            log.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    public String getClaimNameJwt(String token, String claimName) {
+        return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).getPayload().get(claimName).toString();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+}
--- course/src/main/java/com/ead/course/controllers/ModuleController.java
+++ course/src/main/java/com/ead/course/controllers/ModuleController.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -34,14 +35,15 @@ public class ModuleController {
     @Autowired
     CourseService courseService;
 
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @PostMapping("/courses/{courseId}/modules")
-    public ResponseEntity<Object> saveModule(@PathVariable(value = "courseId") UUID courseId, @RequestBody @Valid ModuleDto moduleDto) {
+    public ResponseEntity<Object> saveModule(@PathVariable(value = "courseId") UUID courseId,
+            @RequestBody @Valid ModuleDto moduleDto) {
         log.debug("POST saveModule moduleDto received {} ", moduleDto.toString());
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
-        if(!courseModelOptional.isPresent()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found.");
+        if (!courseModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found.");
         }
-
         var moduleModel = new ModuleModel();
         BeanUtils.copyProperties(moduleDto, moduleModel);
         moduleModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
@@ -49,17 +51,18 @@ public class ModuleController {
         moduleService.save(moduleModel);
         log.debug("POST saveModule moduleId saved {} ", moduleModel.getModuleId());
         log.info("Module saved successfully moduleId {} ", moduleModel.getModuleId());
-        return ResponseEntity.status(HttpStatus.CREATED).body(moduleService);
+        return ResponseEntity.status(HttpStatus.CREATED).body(moduleModel);
     }
 
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
     public ResponseEntity<Object> deleteModule(
             @PathVariable(value = "courseId") UUID courseId,
-            @PathVariable(value = "moduleId") UUID moduleId){
+            @PathVariable(value = "moduleId") UUID moduleId) {
 
         log.debug("DELETE deleteModule moduleId received {} ", moduleId);
         Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourse(courseId, moduleId);
-        if(!moduleModelOptional.isPresent()){
+        if (!moduleModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course.");
         }
         moduleService.delete(moduleModelOptional.get());
@@ -68,6 +71,7 @@ public class ModuleController {
         return ResponseEntity.status(HttpStatus.OK).body("Module deleted successfully");
     }
 
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @PutMapping("/courses/{courseId}/modules/{moduleId}")
     public ResponseEntity<Object> updateModule(
             @PathVariable(value = "courseId") UUID courseId,
@@ -75,7 +79,7 @@ public class ModuleController {
             @RequestBody @Valid ModuleDto moduleDto) {
         log.debug("PUT updateModule moduleDto received {} ", moduleDto.toString());
         Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourse(courseId, moduleId);
-        if(!moduleModelOptional.isPresent()){
+        if (!moduleModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course.");
         }
         var moduleModel = moduleModelOptional.get();
@@ -84,21 +88,24 @@ public class ModuleController {
         moduleService.save(moduleModel);
         log.debug("PUT updateModule moduleId saved {} ", moduleModel.getModuleId());
         log.info("Module updated successfully moduleId {} ", moduleModel.getModuleId());
-        return ResponseEntity.status(HttpStatus.OK).body(moduleService);
+        return ResponseEntity.status(HttpStatus.OK).body(moduleModel);
     }
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/courses/{courseId}/modules")
     public ResponseEntity<Page<ModuleModel>> getAllModules(@PathVariable(value = "courseId") UUID courseId,
-                                                           SpecificationTemplate.ModuleSpec spec,
-                                                           @PageableDefault(page = 0, size = 10, sort = "moduleId", direction = Sort.Direction.ASC) Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllCourse(SpecificationTemplate.moduleCourseId(courseId).and(spec), pageable));
+            SpecificationTemplate.ModuleSpec spec,
+            @PageableDefault(page = 0, size = 10, sort = "moduleId", direction = Sort.Direction.ASC) Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(moduleService.findAllCourse(SpecificationTemplate.moduleCourseId(courseId).and(spec), pageable));
     }
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/courses/{courseId}/modules/{moduleId}")
     public ResponseEntity<Object> getOneModule(@PathVariable(value = "courseId") UUID courseId,
-                                                          @PathVariable(value = "moduleId") UUID moduleId) {
+            @PathVariable(value = "moduleId") UUID moduleId) {
         Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourse(courseId, moduleId);
-        if(!moduleModelOptional.isPresent()){
+        if (!moduleModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module not found for this course.");
         }
         return ResponseEntity.status(HttpStatus.OK).body(moduleModelOptional.get());
