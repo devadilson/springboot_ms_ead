24b4ee9eef1a5b7547a72b9ab687feb43fc4f460
Adilson <didasoueudida@gmail.com>
2023-12-22 12:27:30 -0300

    refact WebSecurityConfig notification

--- notification/src/main/java/com/ead/notification/configs/security/WebSecurityConfig.java
+++ notification/src/main/java/com/ead/notification/configs/security/WebSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -53,12 +53,10 @@ public class WebSecurityConfig {
         .addFilterBefore(authenticationJwtFilter(), UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
-
   @Bean
-  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-    return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    return authConfig.getAuthenticationManager();
   }
-
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
